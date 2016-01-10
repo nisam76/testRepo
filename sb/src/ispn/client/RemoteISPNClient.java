2c7595310619376badeaad44c9ba81c8a2ea16a4
A A <niaa@adprod.bmc.com>
2016-01-10 13:15:42 +0530

    First commit

---
+++ sb/src/ispn/client/RemoteISPNClient.java
@@ -0,0 +1,69 @@
+package ispn.client;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.annotation.ClientCacheEntryCreated;
+import org.infinispan.client.hotrod.annotation.ClientCacheEntryModified;
+import org.infinispan.client.hotrod.annotation.ClientCacheEntryRemoved;
+import org.infinispan.client.hotrod.annotation.ClientListener;
+import org.infinispan.client.hotrod.configuration.Configuration;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.client.hotrod.event.ClientCacheEntryCreatedEvent;
+import org.infinispan.client.hotrod.event.ClientCacheEntryModifiedEvent;
+import org.infinispan.client.hotrod.event.ClientCacheEntryRemovedEvent;
+@ClientListener
+public class RemoteISPNClient {
+	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+	RemoteCache<String, String> cache = null;
+	public RemoteISPNClient() {
+		
+	}
+	
+	
+	public void startClient() {
+		Configuration config = new ConfigurationBuilder().addServer().host("127.0.0.1").port(9999).build();
+		RemoteCacheManager cacheManager = new RemoteCacheManager(config);
+		cache = cacheManager.getCache("dumpster");
+		Calendar cal = Calendar.getInstance();
+		System.out.println(cache.get("K")); // V
+		cache.addClientListener(this);
+	}
+	
+	public static void main(String[] args) {
+		RemoteISPNClient client = new RemoteISPNClient();
+		client.startClient();
+	}
+	
+	@ClientCacheEntryCreated
+	   public void handleCreatedEvent(ClientCacheEntryCreatedEvent<Object> e) {
+		Calendar cal = Calendar.getInstance();
+	      System.out.println(dateFormat.format(cal.getTime()));
+	      System.out.println("Add "+e.getKey()+ "   "+cache.get(e.getKey()));
+	      
+	   }
+
+	   @ClientCacheEntryModified
+	   public void handleModifiedEvent(ClientCacheEntryModifiedEvent<Object> e) throws InterruptedException {
+		   Calendar cal = Calendar.getInstance();
+	      System.out.println(dateFormat.format(cal.getTime()));
+	      System.out.println("Updated key -> "+e.getKey() +" version -> "+e.getVersion()+"  data -> "+cache.get(e.getKey()));
+	      System.out.println("Adding same element again"+e.getKey() +" with version "+e.getVersion());
+	      cache.replaceWithVersion(""+e.getKey(), "NEW VALUE ---", e.getVersion());
+	      Thread.sleep(5000);
+	      System.out.println("Adding same element again"+e.getKey() +" with new version "+1011);
+	     //cache.replaceWithVersion(""+e.getKey(), "NEW VALUE ---",1011);
+	      cache.put(""+e.getKey(), cache.get(e.getKey())+"2222");
+	      //System.out.println(cache.put(""+e.getKey(), cache.get(e.getKey())));
+	   }
+
+	   @ClientCacheEntryRemoved
+	   public void handleRemovedEvent(ClientCacheEntryRemovedEvent<Object> e) {
+		   Calendar cal = Calendar.getInstance();
+		   System.out.println(dateFormat.format(cal.getTime()));
+	      System.out.println("Deleting "+e.getKey());// +cache.get(e.getKey()));
+	   }
+}
---
+++ sb/src/ispn/server/RemoteISPNServer.java
@@ -0,0 +1,89 @@
+package ispn.server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.infinispan.Cache;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.server.hotrod.HotRodServer;
+import org.infinispan.server.hotrod.configuration.HotRodServerConfiguration;
+import org.infinispan.server.hotrod.configuration.HotRodServerConfigurationBuilder;
+
+import ispn.client.RemoteISPNClient;
+
+public class RemoteISPNServer {
+
+	public RemoteISPNServer() {
+		
+	}
+	
+	
+	public void startServer() {
+		// Start a cache manager as usual
+		EmbeddedCacheManager cacheManager = null;
+
+		try {
+//			InputStream in = ClassLoader.getSystemResourceAsStream("C://Users//MDAMODAR//Downloads//ISPN2//hotrod-ispn.xml");
+//		    cacheManager = new DefaultCacheManager(in);
+			
+			cacheManager = new DefaultCacheManager("C://INFINI-HOTROD//ISPN2//ISPN2//hotrod-ispn.xml");
+			RemoteISPNClient eClient = new RemoteISPNClient();
+			
+		} catch(IOException ex) {
+			ex.printStackTrace();
+			System.exit(0);
+		} finally {
+			
+		}
+		
+		//cacheManager = new DefaultCacheManager();
+		
+
+		// Start a server to allow remote access to the cache manager
+		HotRodServerConfiguration serverConfig = new HotRodServerConfigurationBuilder().host("127.0.0.1").port(9999).build();
+		HotRodServer server = new HotRodServer();
+		server.start(serverConfig, cacheManager);
+
+		// Start the example cache
+		Cache<String, String> cache = cacheManager.getCache("dumpster", true);
+		//cache.put("K", "V");
+		//System.out.println(cache.get("K"));
+		int i = 0;
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+		
+		//while(true){
+			
+			try {
+				Thread.sleep(30000);
+				Calendar cal = Calendar.getInstance();
+				System.out.println(dateFormat.format(cal.getTime()));
+				cache.put("K"+i, "V"+i);
+				//cache.put("K"+i, "V"+i);
+				//if(i%5 == 0){
+				//	cache.put("K"+i, "V1"+i);
+					
+					//Thread.sleep(2000);
+					///cache.remove("K"+i);
+					//cache.put("K"+i, "V1"+i);
+					
+				//}
+				//i++;
+				Thread.sleep(10000);
+				cache.put("K"+i, "V"+i);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		//}
+	}
+	
+	
+	public static void main(String[] args) {
+		RemoteISPNServer server = new RemoteISPNServer();
+		server.startServer();		
+	}
+}
